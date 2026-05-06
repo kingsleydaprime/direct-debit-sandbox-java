fac45850db3f40bbdcb52bab10dfb38f2b338094
Kingsley Ihemelandu <kijuchihe@gmail.com>
2026-05-06 14:19:16 +0000

    Initial commit

---
+++ src/main/java/com/itc/direct_debit_sandbox/DirectDebitSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.itc.direct_debit_sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectDebitSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectDebitSandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itc/direct_debit_sandbox/DirectDebitSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itc.direct_debit_sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectDebitSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
